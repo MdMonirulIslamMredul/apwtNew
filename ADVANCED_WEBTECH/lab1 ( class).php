c1c1669aae5266693c1a6f21772d1e4060640104
MdMonirulIslamMredul <monirulislammredul@gmail.com>
2022-05-28 02:40:00 +0600

    f

---
+++ ADVANCED_WEBTECH/lab1 ( class).php
@@ -0,0 +1,40 @@
+<?php
+
+class  Student{
+
+	public $name;
+	public $Id;
+	public $gender;
+
+
+function AddStudent(){
+
+	$this->name =$name;
+	$this->Id =$Id;
+	$this->gender=$gender;
+}
+
+function DeleteStudent(){
+
+}
+
+function ShowStudents(){
+
+}
+
+}
+
+$Rahim = new Student();
+
+
+
+
+class Dept{
+
+
+
+
+}
+
+
+?>
